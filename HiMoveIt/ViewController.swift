d6312afe2e1b5b3969f429d3ce119e81ebbae97e
JaewanJeong <jwmsg0525@gmail.com>
2019-07-16 15:22:34 +0900

    Initial Commit

---
+++ HiMoveIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HiMoveIt
+//
+//  Created by Jaewan Jeong on 16/07/2019.
+//  Copyright © 2019 jwmsg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
