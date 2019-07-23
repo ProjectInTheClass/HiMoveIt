4fb9f1abbdad46ef9ee39940f9df314abfa9ff57
KangKyung <kangkyung@GW-10ui-iMac.local>
2019-07-23 19:47:15 +0900

    dev UI

--- HiMoveIt/ViewControllers/MainViewController.swift
+++ HiMoveIt/ViewControllers/MainViewController.swift
@@ -23,7 +23,18 @@ class MainViewController: UIViewController {
         self.present(recordViewController, animated: true, completion: nil)
     }
     
+    //
+    func loadPreview(){
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Preview", bundle: nil)
+        let previewController = storyBoard.instantiateViewController(withIdentifier: "preview") as! PreviewController
+        self.present(previewController, animated: true, completion: nil)
+    }
 
+    @IBAction func clickTestButton(_ sender: Any) {
+        loadPreview()
+    }
+    //
+    
     @IBAction func clickAddBtn(_ sender: Any) {
         loadRecordView()
     }
