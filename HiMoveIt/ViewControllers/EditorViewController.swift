e93046bad8c44fff8f6f8148a68c5c86a396a02a
JaewanJeong <jwmsg0525@gmail.com>
2019-07-24 22:46:12 +0900

    play mp4 on view.. but it not work well,,

--- HiMoveIt/ViewControllers/EditorViewController.swift
+++ HiMoveIt/ViewControllers/EditorViewController.swift
@@ -8,21 +8,31 @@
 
 import UIKit
 import MobileCoreServices
+import AVFoundation
+import AVKit
 
 class EditorViewController: UIViewController, UINavigationControllerDelegate,UIImagePickerControllerDelegate{
     
     @IBOutlet weak var cancelBtn: UIButton!
+    @IBOutlet weak var playGround: UIView!
     
     var recFileURL:NSURL?
+    var playerLayer:AVPlayerLayer?
+    var playerItem:AVPlayerItem?
+    var playAsset:AVAsset?
+    var player:AVPlayer?
+    var playOnReady:Bool = false;
+    private var playerItemContext = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("URL : "+(recFileURL?.absoluteString)!)
+//        print("URL : "+(recFileURL?.filePathURL?.absoluteString)!)
         // Do any additional setup after loading the view.
     }
     
     func setRecURL(fileURL:NSURL){
         self.recFileURL = fileURL
+        self.setParamVideo()
     }
     
     func goBack(){
@@ -34,7 +44,31 @@ class EditorViewController: UIViewController, UINavigationControllerDelegate,UII
         view.window!.layer.add(transition, forKey: kCATransition)
         self.dismiss(animated: false, completion: nil)
     }
+    func setParamVideo(){
+        playAsset = AVAsset(url: recFileURL! as URL)
+        //playAsset?.loadValuesAsynchronously(forKeys: <#T##[String]#>, completionHandler: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>)
+        self.playerItem = AVPlayerItem(asset: self.playAsset!)
+        self.player = AVPlayer(playerItem:self.playerItem)
+        self.playerLayer = AVPlayerLayer(player: self.player)
+        self.playerLayer!.videoGravity = .resize
+        self.playOnReady = true;
+        
+        //로드하는 시간동안 본 함수가 살아있지 않고 죽어버리면 뒤져버림;;;;;;
+    }
     
+    override func viewDidLayoutSubviews() {            // subView인 playerLayer의 영역 설정
+        super.viewDidLayoutSubviews()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {        // view가 나타날때 player 재생
+        super.viewDidAppear(animated)
+        if(playOnReady){
+            self.playerLayer!.frame = self.playGround.bounds
+            self.playGround.layer.addSublayer(self.playerLayer!)
+            player!.play()
+        }
+    }
     @IBAction func clickCancelBtn(_ sender: Any) {
         goBack()
     }
@@ -44,6 +78,7 @@ class EditorViewController: UIViewController, UINavigationControllerDelegate,UII
     @IBAction func clickDoneBtn(_ sender: Any) {
         self.presentingViewController?.presentingViewController?.dismiss(animated: true, completion: nil)
     }
+    
     /*
     // MARK: - Navigation
 
--- HiMoveIt/ViewControllers/MainViewController.swift
+++ HiMoveIt/ViewControllers/MainViewController.swift
@@ -9,92 +9,16 @@
 import UIKit
 import Photos
 
-class MainViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
-    
-    var numberOfcell: Int = 10
-    let cellIdentifier: String = "cell"
-    
-    @IBOutlet weak var collectionView : UICollectionView!
-    var fetchResult: PHFetchResult<PHAsset>!
-    let imageManager: PHCachingImageManager = PHCachingImageManager()
-    
-    func requestCollection(){
-        let cameraRoll: PHFetchResult<PHAssetCollection> = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .smartAlbumUserLibrary, options: nil)
-        guard let cameraRollCollection = cameraRoll.firstObject else{
-            return
-        }
-        
-        let fetchOptions = PHFetchOptions()
-        fetchOptions.sortDescriptors = [NSSortDescriptor(key:"creationDate", ascending: false)]
-        self.fetchResult = PHAsset.fetchAssets(in: cameraRollCollection, options: fetchOptions)
-    }// 앨범
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.numberOfcell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier:self.cellIdentifier, for: indexPath)
-
-        return cell
-    }
-    
+class MainViewController: UIViewController{
     @IBOutlet weak var addBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let photoAurhorizationStatus = PHPhotoLibrary.authorizationStatus()
-        switch photoAurhorizationStatus{
-        case.authorized:
-            print("접근허가됨")
-            self.requestCollection()
-            self.collectionView.reloadData()
-        case .denied:
-            print("접근 불허")
-        case .notDetermined:
-            print("아직응답하지않음")
-            PHPhotoLibrary.requestAuthorization({(status) in
-                switch status {
-                case .authorized:
-                    print("사용자가 허용함")
-                    self.requestCollection()
-                case .denied:
-                    print("사용자가 불허함")
-                default: break
-                }
-            })
-        case .restricted:
-            print("접근제한")
-        }
-        self .collectionView.reloadData()
     }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let collectionViewCellWithd = collectionView.frame.width / 3 - 1
-        return CGSize(width: collectionViewCellWithd, height: collectionViewCellWithd)
-    } //size
-    
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 1
-    }//up down
-    
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 1
-    }//side
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-
-    
     func loadRecordView(){
         let storyBoard: UIStoryboard = UIStoryboard(name: "Record", bundle: nil)
         let recordViewController = storyBoard.instantiateViewController(withIdentifier: "recordView") as! RecordViewController
         self.present(recordViewController, animated: true, completion: nil)
     }
-    
     func loadPreview(){
         let storyBoard: UIStoryboard = UIStoryboard(name: "Preview", bundle: nil)
         let previewController = storyBoard.instantiateViewController(withIdentifier: "preview") as! PreviewController
