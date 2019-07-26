0eaf0e91cb9f80243daa3470734d23a48727d7f3
ohsungsu <oss5824@naver.com>
2019-07-26 15:11:47 +0900

    gallery picture add

--- HiMoveIt/ViewControllers/MainViewController.swift
+++ HiMoveIt/ViewControllers/MainViewController.swift
@@ -4,27 +4,71 @@
 //
 //  Created by Jaewan Jeong on 16/07/2019.
 //  Copyright © 2019 jwmsg. All rights reserved.
-//	
+//
 
 import UIKit
 import Photos
 
+
 class MainViewController: UIViewController{
-    @IBOutlet weak var addBtn: UIButton!
+    
+    let cellIdentifier: String = "cell"
+    var arrImage = ["1.png","2.png","3.png","4.png","5.png","6.png","7.png","8.png","9.png","10.png","11.png","12.png","13.png","14.png","15.png","16.png","17.png","18.png","19.png"]
+    var urlImg = [URL]()
+    
+    
+
+    @IBOutlet var collectioView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        saveImages()
     }
+    
+    
+    func saveImages() {
+        
+        for imgStr in arrImage{
+            let document = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+            //document안의 url
+            print(document)
+            let imgUrl = document.appendingPathComponent(imgStr,isDirectory: true) //file의 이름 전달
+            print(imgUrl.path)
+            
+            if !FileManager.default.fileExists(atPath: imgUrl.path){
+                do{
+                    try UIImage(named: imgStr)!.pngData()?.write(to: imgUrl)
+                    print("image add successfully")
+                }catch{
+                    print("image not added")
+                }
+            }
+            urlImg.append(imgUrl)
+        }
+        
+    }
+    
+    
+    @IBOutlet weak var addBtn: UIButton!
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
     func loadRecordView(){
         let storyBoard: UIStoryboard = UIStoryboard(name: "Record", bundle: nil)
         let recordViewController = storyBoard.instantiateViewController(withIdentifier: "recordView") as! RecordViewController
         self.present(recordViewController, animated: true, completion: nil)
     }
+    
     func loadPreview(){
         let storyBoard: UIStoryboard = UIStoryboard(name: "Preview", bundle: nil)
         let previewController = storyBoard.instantiateViewController(withIdentifier: "preview") as! PreviewController
         self.present(previewController, animated: true, completion: nil)
     }
-
+    
     @IBAction func clickTestButton(_ sender: Any) {
         loadPreview()
     }
@@ -33,7 +77,38 @@ class MainViewController: UIViewController{
     @IBAction func clickAddBtn(_ sender: Any) {
         loadRecordView()
     }
+    
+}
 
+class imageCollectionViewCellModel: UICollectionViewCell {
+    @IBOutlet var imageEx: UIImageView!
+    
+}
+
+extension MainViewController: UICollectionViewDelegate,UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return urlImg.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! imageCollectionViewCellModel
+        
+        cell.imageEx.image = UIImage(contentsOfFile: urlImg[indexPath.row].path)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let collectionViewCellWithd = collectionView.frame.width / 3 - 1
+        return CGSize(width: collectionViewCellWithd, height: collectionViewCellWithd)
+    } //size
     
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }//up down
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }//side
 }
