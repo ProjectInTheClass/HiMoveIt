f419e4c3a717b12c821b0afca679e9f0b5e3a1a1
ohsungsu <oss5824@naver.com>
2019-07-30 17:18:40 +0900

    deletet button add

--- HiMoveIt/ViewControllers/MainViewController.swift
+++ HiMoveIt/ViewControllers/MainViewController.swift
@@ -1,30 +1,129 @@
+//동영상에서 썸네일 따와서 갤러리에 출력해주고 출력
 //
 //  ViewController.swift
 //  HiMoveIt
 //
 //  Created by Jaewan Jeong on 16/07/2019.
 //  Copyright © 2019 jwmsg. All rights reserved.
-//	
+//
 
 import UIKit
 import Photos
+import AVFoundation
+import MobileCoreServices
+
+let document = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+
+struct UrlAndImage
+{
+    var urlImg = [UIImage]()
+    var urlVideo = [URL]()
+    var indexNumber = [Int]()
+}
+
+
+var structure = UrlAndImage()
 
 class MainViewController: UIViewController{
-    @IBOutlet weak var addBtn: UIButton!
+    
+    let cellIdentifier: String = "cell"
+    
+    var arrImage : [String] = ["01.mov","02.mov","20.mov","21.mov","22.mov","23.mov","24.mov"]
+    
+    
+  
+    @IBOutlet var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        saveImages()
+        
     }
+    
+    
+    
+    func saveImages() {
+        var number = 0
+        while FileManager.default.fileExists(){
+        for imgStr in arrImage{
+            //모든 app의 표준 폴더인 document 폴더를 가지고 온다는 뜻
+            //print(document)
+            let imgUrl: URL = document.appendingPathComponent(imgStr,isDirectory: true)
+            //모든 내용이 저장된 데이터베이스 폴더를 가리키는 경로를 구축합니다.(폴더 안에 해당파일 가르키기)
+            //print(imgUrl.path)
+            let thumbnail : UIImage = self.imgFromVideo(url: imgUrl, at: 8)!
+            
+            structure.urlImg.append(thumbnail)
+            structure.urlVideo.append(imgUrl)
+            structure.indexNumber.append(number)
+            
+            number = number + 1
+        }
+    }
+        
+    }
+    
+    func imgFromVideo(url: URL, at time: TimeInterval) -> UIImage? {
+        let assetUrl = AVURLAsset(url: url)
+        
+        let assetImg = AVAssetImageGenerator(asset: assetUrl)
+        assetImg.appliesPreferredTrackTransform = true
+        assetImg.apertureMode = AVAssetImageGenerator.ApertureMode.encodedPixels
+        
+        let cmTime = CMTime(seconds: time, preferredTimescale: 10)
+        let thumbnailImg: CGImage
+        do {
+            thumbnailImg = try assetImg.copyCGImage(at: cmTime, actualTime: nil)
+        } catch let error {
+            print("Error: \(error)")
+            return nil
+        }
+        
+        return UIImage(cgImage: thumbnailImg)
+    }
+    
+    
+    @IBOutlet weak var addBtn: UIButton!
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func removeImage(indx: Int) {
+        print(indx)
+        print(structure.urlVideo[indx])
+        let deleteUrl : URL = structure.urlVideo[indx]
+        
+        try? FileManager.default.removeItem(at: deleteUrl)
+        
+        structure.urlImg.remove(at: indx)
+        structure.urlVideo.remove(at: indx)
+        structure.indexNumber.remove(at: indx)
+        
+        collectionView.reloadData()
+    }
+    
     func loadRecordView(){
         let storyBoard: UIStoryboard = UIStoryboard(name: "Record", bundle: nil)
         let recordViewController = storyBoard.instantiateViewController(withIdentifier: "recordView") as! RecordViewController
         self.present(recordViewController, animated: true, completion: nil)
     }
+    
     func loadPreview(){
         let storyBoard: UIStoryboard = UIStoryboard(name: "Preview", bundle: nil)
         let previewController = storyBoard.instantiateViewController(withIdentifier: "preview") as! PreviewController
         self.present(previewController, animated: true, completion: nil)
     }
-
+    
+    func loadPreview(cellImage:UIImage, cellVideo:URL, cellNumber:Int){
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Preview", bundle: nil)
+        let previewController = storyBoard.instantiateViewController(withIdentifier: "preview") as! PreviewController
+        previewController.setImage(image: cellImage,url: cellVideo,number: cellNumber)
+        self.present(previewController, animated: true, completion: nil)
+    }
+    
+    
     @IBAction func clickTestButton(_ sender: Any) {
         loadPreview()
     }
@@ -33,7 +132,55 @@ class MainViewController: UIViewController{
     @IBAction func clickAddBtn(_ sender: Any) {
         loadRecordView()
     }
-
     
     
 }
+
+
+//collectionviewcell
+class ImageCollectionViewCellModel: UICollectionViewCell {
+    
+    @IBOutlet weak var imageEx: UIImageView!
+    
+}
+
+
+//extiension
+extension MainViewController: UICollectionViewDelegate,UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return structure.urlImg.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ImageCollectionViewCellModel //dowm size casting
+        
+        cell.imageEx.image = structure.urlImg[indexPath.row]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let collectionViewCellWithd = collectionView.frame.width / 3 - 1
+        return CGSize(width: collectionViewCellWithd, height: collectionViewCellWithd)
+    } //size
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }//up down
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cellImage:UIImage = (collectionView.cellForItem(at: indexPath) as! ImageCollectionViewCellModel).imageEx.image!
+        let cellVideo:URL = structure.urlVideo[indexPath.row]
+        let cellNumber:Int = structure.indexNumber[indexPath.row]
+        
+        self.loadPreview(cellImage: cellImage,cellVideo: cellVideo,cellNumber: cellNumber)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }//side
+}
+
+
+
--- HiMoveIt/ViewControllers/PreviewViewController.swift
+++ HiMoveIt/ViewControllers/PreviewViewController.swift
@@ -1,28 +1,63 @@
 //
-//  PreviewController.swift
-//  HiMoveIt
-//
-//  Created by KangKyung on 22/07/2019.
-//  Copyright © 2019 jwmsg. All rights reserved.
-//
 
 import UIKit
+import AVFoundation
+import AVKit
+import Photos
 
 class PreviewController: UIViewController {
     
     //let text = "공유할 이미지(APNG)에 대한 이름" // 공유할 때 같이 들어가는 텍스트 [ 삭제 가능 ]
     //let URL:NSURL = NSURL(string: "https://www.naver.com")! // 같이 들어가는 URL [ 삭제 가능 ]
-    let image = URL(fileURLWithPath: Bundle.main.path(forResource: "Image", ofType: "png") ?? "") // 일단은 Asset에 있는 예시사진을 넣어두었고, 추후에 넘어온 이미지로 그 대상을 변경시켜줘야함.
+    //let image = URL(fileURLWithPath: Bundle.main.path(forResource: "Image", ofType: "png") ?? "") // 일단은 Asset에 있는 예시사진을 넣어두었고, 추후에 넘어온 이미지로 그 대상을 변경시켜줘야함.
+    
+    
+    
+    @IBOutlet weak var preViewImage: UIImageView!
+    
+    var image: UIImage = UIImage()
+    var imageUrl: URL!
+    var index: Int?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let player = AVPlayer(url: imageUrl)
+        let playerViewController = AVPlayerViewController()
+        playerViewController.player = player
+        
+        self.present(playerViewController, animated: true){
+            playerViewController.player?.play()
+        }
+    }
+    
+    func setImage(image: UIImage, url: URL,number: Int){
+        self.image = image
+        self.imageUrl = url
+        self.index = number
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        preViewImage.image = image
+    }
+    
+    func loadMainView(){
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let mainViewController = storyBoard.instantiateViewController(withIdentifier: "main") as! MainViewController
+        mainViewController.removeImage(indx: index!)
+        self.present(mainViewController, animated: true, completion: nil)
+    }
+    
     @IBAction func btnShare(_ sender: Any) {
-        // 공유할 때 넘어가는 정보
-        let vc = UIActivityViewController(activityItems: [image], applicationActivities: []) // text, URL,
-
+        //let videoUrl = URL(fileURLWithPath: Bundle.main.path(forResource: "FastTyping", ofType: "mp4") ?? "")
+        // 공유할 때 넘어가는 정보들. 현재는 3가지로 해둠
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        //let activityVC = UIActivityViewController(activityItems: [videoUrl] , applicationActivities: nil)
+        //present(vc, animated: true, completion: nil)
+        
         if let popoverController = vc.popoverPresentationController {
             popoverController.sourceView = self.view
             popoverController.sourceRect = self.view.bounds
@@ -32,12 +67,17 @@ class PreviewController: UIViewController {
     }
     
     @IBAction func btnPlay(_ sender: Any) {
-        // 해당 이미지를 재생시키는 코드 -> 썸네일
+        let player = AVPlayer(url: imageUrl)
+        let playerViewController = AVPlayerViewController()
+        playerViewController.player = player
+        
+        self.present(playerViewController, animated: true){
+            playerViewController.player?.play()
+        }
     }
     
     @IBAction func btnDelete(_ sender: Any) {
-        // 해당 이미지를 삭제하는 코드
-        
+        loadMainView()
         self.dismiss(animated: true, completion: nil)
     }
     
@@ -46,3 +86,6 @@ class PreviewController: UIViewController {
     }
     
 }
+
+
+
